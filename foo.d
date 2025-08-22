ceaf646b22438891babde81f56b41b53a63f2eba
Mason Remaley <mason@anthropicstudios.com>
2025-08-21 21:01:31 -0700

    Revert "Writes input file to deps"
    This reverts commit e4e10f982f9dda4d974767a29c601b4f5ba3e229.

--- foo.d
+++
@@ -1,6 +0,0 @@
-out.spv: \
-    ../2Pew/src/shaders/box_blur_moving_avg.comp.glsl \
-    ../2Pew/src/shaders/interface.glsl \
-    ../gbms/include/gbms/c.glsl \
-    ../gbms/include/gbms/mod_timer.glsl \
-    ../gbms/include/gbms/c.glsl 
